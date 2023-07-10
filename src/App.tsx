b3800e7ce3351450d825b719f9ff13ec3e00e5ef
victorb999 <victor.b_@hotmail.com>
2023-07-10 11:23:52 -0300

    chore: lint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import "./styles/global.css";
-import { Header } from './components/Header/Header';
+import './styles/global.css'
+import { Header } from './components/Header/Header'
 
 function App() {
   return (
     <div className="App">
       <Header />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography } from '@mui/material'
 
-  const Header = () => {
-    return (
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" >
-            GALLERY SAGA
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    );
-  };
+const Header = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" >
+          GALLERY SAGA
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-  export {Header}
+export { Header }
